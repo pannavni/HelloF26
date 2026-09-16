9135fc711a81fbfc9c8db262cd04ff6ffb97d2f6
Navnit Pannu <pannavni@sheridancollege.ca>
2026-09-16 10:33:43 -0400

    First commit

---
+++ src/hellof26/StudentList.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hellof26;
+
+/**
+ *
+ * @author pannu
+ */
+public class StudentList{
+    public static void main(String[] args){
+        Student[] studentList = new Student[3];
+        
+        Student s1 = new Student("1","Ronak");
+        studentList[0]=s1;
+        studentList[1]=new Student("2","Alice");
+        studentList[2]=new Student("3","Bob");
+       
+        for (Student studentList1 : studentList) {
+            System.out.println(studentList1.getsName());
+        }
+        
+        
+    }
+    
+    
+}
